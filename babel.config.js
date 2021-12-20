5d938d74b30250b28359bd1cf63b2f5f8dbd1d7d
xivilay <xivilay@gmail.com>
2021-12-20 12:25:37 +0100

    forked from chord recognition ui

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    presets: [['@babel/preset-env', { modules: 'umd' }], '@babel/preset-react'],
+    plugins: [
+        '@babel/plugin-proposal-class-properties',
+        ['@babel/plugin-transform-runtime', { absoluteRuntime: false, corejs: 3 }],
+    ],
+};
